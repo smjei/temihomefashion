5e55c69d7a0adcd3f1769b32138288bd4d99cb8e
smjei <109876376+smjei@users.noreply.github.com>
2023-10-18 11:51:14 +0200

    Update urls.py

--- temifashion/urls.py
+++ temifashion/urls.py
@@ -24,5 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('temihomefashion.urls')),
     path('', TemplateView.as_view(template_name="index.html"), name="index"),
-    path('send-form-email/', views.SendFormEmail.as_view(), name='send_email'),
 ]
